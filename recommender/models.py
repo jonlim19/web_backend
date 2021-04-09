2a91efccd605cb2915bb723bf5360cfd7438581d
Jonathan Lim <j.lim-kean-lee@mycit.ie>
2021-04-09 10:02:09 +0100

    Profile fixed

--- recommender/models.py
+++ recommender/models.py
@@ -30,12 +30,3 @@ class University(models.Model):
 
     def __str__(self):
         return self.Institution_Name
-
-class User(models.Model):
-    firstname = models.CharField(max_length=30)
-    lastname = models.CharField(max_length=30)
-    email = models.EmailField(max_length=254)
-    password = models.CharField(max_length=254)
-
-    def __str__(self):
-        return self.email
